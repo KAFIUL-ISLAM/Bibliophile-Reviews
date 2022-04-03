f7dc00de0e54414d6592651e8201b7c982b3abc4
KafipHero <alkafi28@gmail.com>
2022-04-03 22:29:43 +0600

    primary components added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import Header from './Components/Header/Header';
+import Reviews from './Components/Reviews/Reviews';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/dashboard'></Route>
+        <Route path='/blogs'></Route>
+        <Route path='/about'></Route>
+        <Route path='*' element={<h1 className='text-8xl'>404 NOT FOUND</h1>}></Route>
+      </Routes>
     </div>
   );
 }
