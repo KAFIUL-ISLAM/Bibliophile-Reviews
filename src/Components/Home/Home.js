1ab809963c6b2c93d4e6bfef9514a1cbaa57cf09
KafipHero <alkafi28@gmail.com>
2022-04-04 12:33:15 +0600

    intro section added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,7 +3,21 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>This is Home</h1>
+            <div className='grid grid-cols-2 gap-4 mx-10 place-items-center'>
+                <div className='p-12'>
+                    <img className='rounded' src="/book.png" alt="The Alchemist-Paulo Coelho" />
+                </div>
+                <div className=''>
+                    <h1 className='text-6xl font-bold'>This is the Title</h1>
+                    <p className='text-lg font-semi-bold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ut rem commodi, quo nobis omnis, praesentium quae quis consectetur cupiditate id doloribus eveniet. Aliquam alias consequuntur iusto omnis aspernatur nesciunt.</p>
+                </div>
+            </div>
+            <div>
+                <h1>Reviews</h1>
+                <div>
+                    
+                </div>
+            </div>
         </div>
     );
 };
